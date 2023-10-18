f1e071d651cb72de73fb0dbe46c3f435b62e457d
Alen Scaria <107207581+Alen-Scaria1516@users.noreply.github.com>
2023-10-18 08:41:21 +0530

    Update One-way-chat.c

--- Lab 4/One-way-chat.c
+++ Lab 4/One-way-chat.c
@@ -31,3 +31,36 @@ flag = 1;
 } while(flag != 1); // do while loop for continuous communication 
 return 0; 
 }
+
+/*part 2*/
+#include<stdio.h> 
+#include<stdlib.h> 
+#include<errno.h> 
+#include<string.h> 
+#include<fcntl.h> 
+#include<sys/types.h> 
+#include<sys/stat.h> 
+#include<unistd.h> 
+#define FIFO_NAME "myfile.txt" 
+int main(void) 
+{ 
+char s[300]; 
+int num,fd; 
+mkfifo(FIFO_NAME, 0666); 
+printf("waiting for writers...\n"); 
+fd = open(FIFO_NAME, O_RDONLY); // open the read end  
+printf("got a writer\n");int flag = 0; 
+do 
+{ 
+char s[300]; int num;  
+ 
+if((num = read(fd,s,300)) == -1) 
+perror("read"); 
+else{ 
+s[num]= '\0'; 
+printf("Reader read %d bytes\n",num);} 
+if(s == 'exit') 
+flag = 1; 
+}while(flag == 0); 
+return 0; 
+}
