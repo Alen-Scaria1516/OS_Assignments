bf08f589540750348d740de7a3d7b04ffd63eca5
Alen Scaria <107207581+Alen-Scaria1516@users.noreply.github.com>
2023-10-18 08:40:43 +0530

    Create One-way-chat.c

---
+++ Lab 4/One-way-chat.c
@@ -0,0 +1,33 @@
+#include<stdio.h> 
+#include<stdlib.h> 
+#include<errno.h> 
+#include<string.h> 
+#include<fcntl.h> 
+#include<sys/types.h> 
+#include<sys/stat.h> 
+#include<unistd.h> 
+#define FIFO_NAME "myfile.txt" 
+ 
+int main(void) 
+{ 
+char s[300]; 
+int num,fd;  
+mkfifo(FIFO_NAME, 0666); 
+printf("waiting for readers...\n"); // reader program 
+fd = open(FIFO_NAME, O_WRONLY); // will open the writing end 
+only --O_WRONLY 
+printf("got a reader--type some stuff\n");int flag = 0; 
+do 
+{ int num; char s[300];  
+gets(s); // the typed stuff will be stored in char s 
+ 
+if((num = write(fd,s, strlen(s))) == -1) 
+perror("write"); 
+else 
+printf("Writer wrote %d bytes\n",num);// if no error exists it will print 
+the input  
+if(s == 'exit') // if the input will be exit it will break and chat will end 
+flag = 1; 
+} while(flag != 1); // do while loop for continuous communication 
+return 0; 
+}
